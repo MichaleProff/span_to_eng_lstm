e4b62a54480644871efe1e2d0ad0149d45ec5935
RustyWWIBayonet <michaleproff@gmail.com>
2024-04-11 16:29:10 -0400

    halfway

--- mine/my_preprocess.py
+++ mine/my_preprocess.py
@@ -11,3 +11,26 @@ target_docs = []
 input_tokens = set()
 target_tokens = set()
      
+for line in lines:
+     input_doc, target_doc = line.split('\t')[:2]
+     input_docs.append(input_doc)
+
+     target_doc = " ".join(re.findall(r"[\w']+|[^\s\w]", target_doc))
+     target_doc = '<START> ' + target_doc + ' <END>'
+     target_docs.append(target_doc)
+
+     for token in re.findall(r"[\w]+|[^\s\w]", input_doc):
+          if token not in input_tokens:
+               input_tokens.add(token)
+     for token in target_doc.split():
+          if token not in target_tokens:
+               target_tokens.add(token)
+
+input_tokens = sorted(list(input_tokens))
+target_tokens = sorted(list(target_tokens))
+
+num_encoder_tokens = len(input_tokens)
+num_decoder_tokens = len(target_tokens)
+
+max_encoder_seq_length = max([len(re.findall(r"[\w]+|[^\s\w]", input_doc)) for input_doc in input_docs])
+max_decoder_seq_length = max([len(re.findall(r"[\w]+|[^\s\w]", target_doc)) for target_doc in target_docs])
